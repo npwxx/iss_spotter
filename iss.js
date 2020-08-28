e595e4545797c6327f46cb017f94f4551b3023d0
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-28 04:03:30 +0000

    linted & removed some comments

--- iss.js
+++ iss.js
@@ -1,13 +1,5 @@
 const request = require('request');
 
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const fetchMyIP = function(callback) {
   const url = 'https://api.ipify.org?format=json';
   request(url, (error, response, body) => {
